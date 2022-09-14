bd1f55cafeba439e533ca97a7443ab2b7fe4e27f
Mahesh <mmiles3.1415@gmail.com>
2022-09-14 12:22:23 +0530

    small edit

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ def index():
 
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1,6)
-    joblib_path = path.join(path.dirname(path.abspath(__file__)), "heart.joblib")
-    print(joblib_path)
+    joblib_path = "./heart.joblib"
     loaded_model = joblib.load(joblib_path)
     result = loaded_model.predict(to_predict)
     return 'You are safe' if int(result[0]) else 'You have a risk of heart disease'
@@ -34,8 +33,7 @@ def result():
             to_predict_list = list(map(float, to_predict_list))
         except:
             return render_template("index.html",prediction="Enter complete data")
-    result = ValuePredictor(to_predict_list)
-    prediction = str(result)
+    prediction = str(ValuePredictor(to_predict_list))
     return render_template("index.html",prediction=prediction)
 
 if __name__ == "__main__":
